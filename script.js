14fff4e884290ca3b8ee9750e7da3a8ad3d0e375
samuelindberg <samuel.lindberg01@icloud.com>
2022-11-17 17:36:33 +0100

    övre raden blir film-array nu, fel färg dock

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 const yourself = document.getElementById("yourself");
+const rateHover = document.getElementById("intro-text");
+const yourselfMovies = ["Goodfellas","The Big Short"];
+let movieNumber=0;
+let movieRateText = "rate "+yourselfMovies[movieNumber]+" yourself";
+let rate = "rate";
+
+
+rateHover.addEventListener("mouseover", () => {  
+    rateHover.style.color="#FE4500";
+    // if(movieNumber<yourselfMovies){
+        if(yourselfMovies.length>movieNumber){
+            if(movieNumber==0){
+                rateHover.innerText ="like " + yourselfMovies[movieNumber]; 
+            }
+            // }else{
+            //     if(movieNumber<=yourselfMovies){
+                 else{
+                    rateHover.innerText ="or " + yourselfMovies[movieNumber]; 
+                 } 
+        }
+        
+        //     }
+        // }
+         
+    // }
+
+    console.log(movieNumber);
+    movieNumber++
+});
+rateHover.addEventListener("click", () => {
+    rateHover.innerText ="or " + yourselfMovies[movieNumber];
+    movieNumber++
+});
+
+    
+
